1b1a2155bcadf8d9887bc02dcd2cbc91e85cdba6
Tinchocw <mcwikla@fi.uba.ar>
2024-11-04 16:59:11 -0300

    feat: validation of register json structure

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -37,6 +37,8 @@ export class MetricController {
         if (!metricsData.type) {
             throw new ValidationError('type', 'Invalid type', 'INVALID_TYPE');
         }
+
+        this.validateMetrics(metricsData);
     }
 
     private validateDate(date: string | Date) {
@@ -44,5 +46,48 @@ export class MetricController {
         if (isNaN(new Date(date).getTime()) || !date) {
             throw new ValidationError('createdAt', 'Invalid createdAt', 'INVALID_CREATED_AT');
         }
+    }
+
+    private validateMetrics(metricsData: MetricDataDto) {
+        if (Object.keys(metricsData.metrics).length === 0) {
+            throw new ValidationError('metrics', 'Invalid metrics', 'INVALID_METRICS');
+        }
+        if(metricsData.type === 'register' ) {
+            this.validateRegisterMetrics(metricsData.metrics);
+        }
+
+    }
+
+    private validateRegisterMetrics(metrics: Record<string, never>){
+
+
+        // Validar que contenga los campos necesarios
+        // Validar que 'count' sea un número y exista
+        if ('count' in metrics) {
+            if (typeof metrics.count !== 'number') {
+                throw new ValidationError('metrics', '"count" must be a number', 'INVALID_COUNT');
+            }
+        } else {
+            throw new ValidationError('metrics', '"count" is required', 'MISSING_FIELD');
+        }
+
+        // Validar que 'registration_time' sea un número y exista
+        if ('registration_time' in metrics) {
+            if (typeof metrics.registration_time !== 'number') {
+                throw new ValidationError('metrics', '"registration_time" must be a number', 'INVALID_REGISTRATION_TIME');
+            }
+        } else {
+            throw new ValidationError('metrics', '"registration_time" is required', 'MISSING_FIELD');
+        }
+
+        // Validar que 'success' sea un booleano y exista
+        if ('success' in metrics) {
+            if (typeof metrics.success !== 'boolean') {
+                throw new ValidationError('metrics', '"success" must be a boolean', 'INVALID_SUCCESS');
+            }
+        } else {
+            throw new ValidationError('metrics', '"success" is required', 'MISSING_FIELD');
+        }
+
     }
 }
